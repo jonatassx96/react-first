701c8d64f86beb503cb5c652b31157df62feaf9d
jonatassx96 <jonatas.sx96@gmail.com>
2022-11-23 17:08:08 -0300

    chore:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
+import People from "./assets/user-inicio.svg";
+import Arrow from "./assets/arrow.svg";
+import {
+  Container,
+  Image,
+  ContainerItens,
+  H1,
+  InputLabel,
+  Input,
+  Button,
+} from "./styles";
 
-const App = ( ) => {
-  return <div>Hello!!</div>
+function App() {
+  const users = [
+    { id: Math.random(), name: "Maria", age: 33 },
+    { id: Math.random(), name: "Jonatas", age: 26 },
+  ];
+
+  return (
+    <Container>
+      <Image alt="Logo-imagem" src={People} />
+      <ContainerItens>
+        <H1>Olá!</H1>
+        <InputLabel>Nome</InputLabel>
+        <Input placeholder="Nome"></Input>
+
+        <InputLabel>Idade</InputLabel>
+        <Input placeholder="Idade"></Input>
+
+        <Button>
+          Cadastrar
+          <img alt="Seta para direita" src={Arrow} />
+        </Button>
+
+        <ul>
+          {users.map((user) => (
+            <li>
+              {user.name} - {user.age}
+            </li>
+          ))}
+        </ul>
+      </ContainerItens>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+import Background from "./assets/bg-image.svg";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+  background: url("${Background}");
+  background-size: cover;
+`;
+
+export const Image = styled.img`
+  margin-top: 30px;
+`;
+
+export const ContainerItens = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: linear-gradient(
+    157.44deg,
+    rgba(255, 255, 255, 0.6) 0.84%,
+    rgba(255, 255, 255, 0.6) 0.85%,
+    rgba(255, 255, 255, 0.15) 100%
+  );
+  border-radius: 61px 61px 0px 0px;
+  padding: 50px 36px;
+  height: 100vh;
+`;
+
+export const H1 = styled.h1`
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 34px;
+  line-height: 40px;
+  text-align: center;
+  color: #ffffff;
+  margin-bottom: 50px;
+`;
+
+export const InputLabel = styled.p`
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 22px;
+  color: #eeeeee;
+  letter-spacing: -0.408px;
+  padding-left: 25px;
+`;
+
+export const Input = styled.input`
+  width: 342px;
+  height: 58px;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 14px;
+  border: none;
+  margin-bottom: 34px;
+  padding-left: 25px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 28px;
+
+  color: #ffffff;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  width: 342px;
+  height: 74px;
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: 14px;
+  margin-top: 30px;
+  color: #eeeeee;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+  &:active {
+    opacity: 0.5;
+  }
+`;
